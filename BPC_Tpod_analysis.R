a0239bd4ca8a4b78aa4844c936a38987939596e6
aryankumarr216-max <aryankumarr216@gmail.com>
2026-06-01 10:05:07 -0400

    Add files via upload

--- BPC_Tpod_analysis.R
+++ BPC_Tpod_analysis.R
@@ -20,7 +20,7 @@ df_BPC <- read.delim(
   "BPC_bmdexpress_input_log2_transformed_williams_0.05_NOMTC_foldfilter1.5_BMD_filtered.txt",
   sep = "\t",
   header = TRUE,
-  skip = 53
+  skip = 55
 )
 
 #============================================================================
@@ -61,13 +61,14 @@ LCRD.2 <- function(bmc, probe, cut = 1.66) {
 
 #bmc = bmd 
 
-bmc <- as.list(df$`Best.BMD`)
-bmc_v <- unlist(bmc)
-probe <-as.list(df$`Probe.ID`)
-probe_v <- unlist(probe)
+bmc_BPC <- as.list(df_BPC$"Best.BMD")
+bmc_v_BPC <- unlist(bmc_BPC)
+probe_BPC <-as.list(df_BPC$`Probe.ID`)
+probe_v_BPC <- unlist(probe_BPC)
 
 
-LCRD.2(bmc_v,probe_v)
+LCRD2_BPC_table <- LCRD.2(bmc_v_BPC,probe_v_BPC)
+LCRD2_BPC_Tpod <- LCRD2_BPC_table$BMC
 
 ##################################################################
 #FORMULA FOR FIRST MODE-Test run
@@ -141,7 +142,9 @@ First.Mode <- function(bmc, log_base = 10, bw = "nrd0", min.size = 0.1) {
   data.frame(First_Mode = first_mode_linear, Peak_Size = sizes[1], Total_Modes = length(modes))
 }
 
-First.Mode(bmc_v)
+First_mode_BPC_table <- First.Mode(bmc_v_BPC)
+First_mode_BPC_Tpod <- First_mode_BPC_table$First_Mode
+
 ##################################################################
 #tpod from 20th gene:
 
@@ -154,9 +157,9 @@ nth_gene_bootstrap <- function(x, seed = 1, nth_gene = 20, repeats = 2000) {
   return(quantile(boot_nth_gene, probs = c(0.025, 0.5, 0.975)))
 }
 
-
-nth_gene_bootstrap(bmc_v, seed = 1, nth_gene = 20, repeats = 2000)
-
+nth_gene_bootstrap(bmc_v_BPC, seed = 1, nth_gene = 20, repeats = 2000)
+twenty_gene_BPC_table <- nth_gene_bootstrap(bmc_v_BPC, seed = 1, nth_gene = 20, repeats = 2000)
+twenty_gene_BPC_Tpod <- as.numeric(twenty_gene_BPC_table[2])
 ##################################################################
 
 #tpod from 10 percentile
@@ -171,5 +174,192 @@ nth_percent_bootstrap <- function(x, seed = 1, nth_percent = 10, repeats = 2000)
   return(quantile(boot_nth_percent, probs = c(0.025, 0.5, 0.975)))
 }
 
-nth_percent_bootstrap (bmc_v, seed = 1, nth_percent = 10, repeats = 2000)
+nth_percent_bootstrap (bmc_v_BPC, seed = 1, nth_percent = 10, repeats = 2000)
+tenth_percentile_BPC_table <- nth_percent_bootstrap (bmc_v_BPC, seed = 1, nth_percent = 10, repeats = 2000)
+
+tenth_percentile_BPC_Tpod <- as.numeric(tenth_percentile_BPC_table[2])
+###############################################################################
+#DRG = display on graph = number of genes after flitering 
+
+#Defined category Analysis Tpod (lowest median BMD)
+
+
+setwd("C:/Users/KumarA/Downloads/R_Stuff/Actual Analysis/FINAL_tpod/Generic/Defined Category analysis")
+
+
+df_BPC_defCAT <- read.delim(
+  "BPC_bmdexpress_input_log2_transformed_williams_0.05_NOMTC_foldfilter1.5_BMD_null_DEFINED-CatMapFile- Gallus_true_true_conf0.5_1_filtered.txt",
+  sep = "\t",
+  header = TRUE,
+  skip = 70
+)
+
+BPC_Cat_tpod <- min(df_BPC_defCAT$BMD.Median, na.rm = TRUE)
+
+
+
+
+
+###############################################################################
+#tpod summary
+
+BPC_tpod_summary <- data.frame(LCRD2 = LCRD2_BPC_Tpod, First_Mode = First_mode_BPC_Tpod, twentieth_gene = twenty_gene_BPC_Tpod, tenth_percentile = tenth_percentile_BPC_Tpod, Lowest_BMC_median_Categorical = BPC_Cat_tpod )
+###############################################################################
+#graph 
+
+library(ggplot2)
+
+
+ggplot(df_BPC, aes(x = Best.BMD)) +
+  geom_histogram(binwidth = 0.5, color = "black", fill = "steelblue") +
+  labs(x = "Best BMD", y = "Count")
+
+
+#Transform data from wide to long
+library(tidyr)
+
+df_long <- pivot_longer(
+  df_BPC,
+  cols = c(Best.BMD, Best.BMDL),
+  names_to = "variable",
+  values_to = "value"
+)
+
+ggplot(df_long, aes(x = value, fill = variable)) +
+  geom_histogram(
+    position = "dodge",
+    binwidth = 0.5,
+    color = "black"
+  ) +
+  scale_fill_manual(values = c("tomato", "steelblue"))
+
+
+#Bring in multiple files 
+files <- list.files(pattern = "_bmdexpress_input_log2_transformed_williams_0.05_NOMTC_foldfilter1.5_BMD_filtered.txt")
+library(dplyr)
+all <- lapply(files, function(f) read.csv(f) %>% mutate(source = f)) %>% bind_rows()
+
+
+#Histogram
+ggplot(df_BPC, aes(x = Best.BMD)) +
+  geom_histogram(binwidth = 1)
+
+#box Plot
+ggplot(df_long, aes(x = variable, y = value, fill = variable)) +
+  geom_boxplot()
+
+# Calculate the 10th percentile
+p10 <- quantile(df_BPC$Best.BMD, 0.10, na.rm = TRUE)
+
+# Density plot with percentile marker
+ggplot(df_BPC, aes(x = Best.BMD)) +
+  geom_density(fill = "lightblue", alpha = 0.4) +
+  geom_vline(xintercept = p10,
+             linetype = "dashed",
+             color = "red") +
+  annotate("text",
+           x = p10,
+           y = 0.02,
+           label = "10th percentile",
+           hjust = -0.1) +
+  labs(x = "Best.BMD",
+       y = "Density")
+
+
+
+df <- df_BPC %>%
+  mutate(logBestBMD = log10(Best.BMD))
+
+ggplot(df, aes(x = logBestBMD)) +
+  geom_density() +
+  labs(x = "log10(Best.BMD)")
+
+
+
+
+
+
+#tpod variables: LCRD2_BPC_Tpod, LCRD2_BPC_Tpod, twenty_gene_BPC_Tpod, tenth_perecentile_BPC_Tpod, BPC_Cat_tpod
+
+df <- df_BPC %>%
+  mutate(logBestBMD = log10(Best.BMD))
+
+ggplot(df, aes(x = logBestBMD)) +
+  geom_density(fill = "lightblue", alpha = 0.4) +
+  
+  geom_vline(xintercept = log10(p10),
+             linetype = "dashed",
+             color = "red") +
+  
+  geom_vline(xintercept = log10(LCRD2_BPC_Tpod),
+             linetype = "dashed",
+             color = "blue") +
+  
+  geom_vline(xintercept = log10(First_mode_BPC_Tpod),
+             linetype = "dashed",
+             color = "green") +
+  
+  geom_vline(xintercept = log10(twenty_gene_BPC_Tpod),
+             linetype = "dashed",
+             color = "purple") +
+  
+  geom_vline(xintercept = log10(BPC_Cat_tpod),
+             linetype = "dashed",
+             color = "pink") +
+  
+  labs(x = "log10(Best.BMD)",
+       y = "Density")
+
+######################################################
+#Separate lines
+#dev.off()
+
+library(dplyr)
+library(ggplot2)
+
+df <- df_BPC %>%
+  mutate(logBestBMD = log10(Best.BMD))
+
+# Dataframe for TPoD lines
+tpod_df <- data.frame(
+  value = log10(c(
+    p10,
+    LCRD2_BPC_Tpod,
+    First_mode_BPC_Tpod,
+    twenty_gene_BPC_Tpod,
+    BPC_Cat_tpod
+  )),
+  
+  label = c(
+    paste0("10th Percentile = ", round(p10, 3)),
+    paste0("LCRD = ", round((LCRD2_BPC_Tpod), 3)),
+    paste0("First Mode = ", round((First_mode_BPC_Tpod), 3)),
+    paste0("20th Gene = ", round((twenty_gene_BPC_Tpod), 3)),
+    paste0("Category = ", round((BPC_Cat_tpod), 3))
+  )
+)
+
+ggplot(df, aes(x = logBestBMD)) +
+  
+  geom_density(fill = "lightblue", alpha = 0.4) +
+  
+  geom_vline(data = tpod_df,
+             aes(xintercept = value,
+                 color = label),
+             linetype = "dashed",
+             linewidth = 1) +
+  
+  geom_text(data = tpod_df,
+            aes(x = value + 0.05,
+                y = c(0.02, 0.04, 0.06, 0.08, 0.10),
+                label = round(value, 3),
+                color = label),
+            angle = 90,
+            hjust = 0,
+            show.legend = FALSE) +
+  
+  labs(x = "log10(Best.BMD)",
+       y = "Density",
+       color = "TPoD Methods",
+       title = "                                            Distribution of Best BMD Values for BPC")
 
